fd601a3d7950b49fb19a5e70c7b4dde33ba9170b
Nacho <ignacio.boada@outlook.com>
2018-09-24 05:26:04 -0300

    Agregados comentarios con ordenes de complejidad

--- Variante_Gale_Shapley.py
+++ Variante_Gale_Shapley.py
@@ -9,24 +9,24 @@ def variante_Gale_Shapley(recitales_preferencias, bandas_preferencias, X, Y):
 	recitales_libres = []
 	bandas_por_recital = {}
 	recitales_por_banda = {}
-	for i in range(1, N+1):
+	for i in range(1, N+1): #O(N)
 		recitales_libres.append(i)
 		bandas_por_recital[i] = [] 
-	for j in range(1, M+1):	
+	for j in range(1, M+1):	 #O(M)
 		recitales_por_banda[j] = []
 	recitales_ult_propuesto = [0] * N
 	
-	while len(recitales_libres)>0:
-		recital_act = recitales_libres.pop()
-		for i in range(recitales_ult_propuesto[recital_act-1], M):
+	while len(recitales_libres)>0: #O(N)
+		recital_act = recitales_libres.pop() #O(1)
+		for i in range(recitales_ult_propuesto[recital_act-1], M): #O(M)
 			banda_act = recitales_preferencias[recital_act][i]
 			if len(bandas_por_recital[recital_act]) == X:
 				break
 			if len(recitales_por_banda[banda_act]) < Y:
 				recitales_por_banda[banda_act].append(recital_act)
 				bandas_por_recital[recital_act].append(banda_act)
-			elif banda_prefiere_recital_nuevo(banda_act, recital_act, recitales_por_banda, bandas_preferencias):
-				recital_viejo = reemplazar_recital_viejo_por_nuevo(banda_act, recital_act, recitales_por_banda, bandas_por_recital, bandas_preferencias)
+			elif banda_prefiere_recital_nuevo(banda_act, recital_act, recitales_por_banda, bandas_preferencias): #O(Y^2)
+				recital_viejo = reemplazar_recital_viejo_por_nuevo(banda_act, recital_act, recitales_por_banda, bandas_por_recital, bandas_preferencias)#O(Y^2)
 				if recitales_ult_propuesto[recital_viejo-1] < M:
 					recitales_libres.append(recital_viejo)
 			recitales_ult_propuesto[recital_act-1] += 1
@@ -34,17 +34,17 @@ def variante_Gale_Shapley(recitales_preferencias, bandas_preferencias, X, Y):
 	imprimir_resultados(bandas_por_recital)
 	
 
-def banda_prefiere_recital_nuevo(banda_act, recital_act, recitales_por_banda, bandas_preferencias):
-	for x in range(len(bandas_preferencias[banda_act])-1, 0, -1):
-		if bandas_preferencias[banda_act][x] in recitales_por_banda[banda_act]:
+def banda_prefiere_recital_nuevo(banda_act, recital_act, recitales_por_banda, bandas_preferencias):  #O(Y^2)
+	for x in range(len(bandas_preferencias[banda_act])-1, 0, -1): #O(Y)
+		if bandas_preferencias[banda_act][x] in recitales_por_banda[banda_act]: #O(Y)
 			return True
 		if bandas_preferencias[banda_act][x] == recital_act:
 			return False
 	
 	
-def reemplazar_recital_viejo_por_nuevo(banda_act, recital_act, recitales_por_banda, bandas_por_recital, bandas_preferencias):
-	for x in range(len(bandas_preferencias[banda_act])-1, 0, -1):
-		if bandas_preferencias[banda_act][x] in recitales_por_banda[banda_act]:
+def reemplazar_recital_viejo_por_nuevo(banda_act, recital_act, recitales_por_banda, bandas_por_recital, bandas_preferencias):#O(Y^2)
+	for x in range(len(bandas_preferencias[banda_act])-1, 0, -1): #O(Y)
+		if bandas_preferencias[banda_act][x] in recitales_por_banda[banda_act]: #O(Y)
 			recital_viejo = bandas_preferencias[banda_act][x]
 			recitales_por_banda[banda_act].remove(recital_viejo)
 			recitales_por_banda[banda_act].append(recital_act)
